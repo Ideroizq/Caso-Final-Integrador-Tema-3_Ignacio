640818d0e5ecce0b7045712e7e8a8bd478fa0b41
José Alfredo Rubert Pérez <jrubeper@myuax.com>
2024-11-26 11:43:24 +0100

    arreglo errores

---
+++ cmake-build-debug/CMakeFiles/Caso_Final_Integrador_Tema_3_Ignacio.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Caso_Final_Integrador_Tema_3_Ignacio.dir/main.cpp.obj"
+  "CMakeFiles/Caso_Final_Integrador_Tema_3_Ignacio.dir/main.cpp.obj.d"
+  "Caso_Final_Integrador_Tema_3_Ignacio.exe"
+  "Caso_Final_Integrador_Tema_3_Ignacio.exe.manifest"
+  "Caso_Final_Integrador_Tema_3_Ignacio.pdb"
+  "libCaso_Final_Integrador_Tema_3_Ignacio.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Caso_Final_Integrador_Tema_3_Ignacio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Caso_Final_Integrador_Tema_3_Ignacio.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Caso_Final_Integrador_Tema_3_Ignacio.
+# This may be replaced when dependencies are built.
